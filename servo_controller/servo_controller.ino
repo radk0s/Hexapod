8a89522348218376113eea6281c91d2f6620b905
radk0s <radk0slaw@gmail.com>
2014-01-08 22:48:24 +0100

    added legs

--- servo_controller/servo_controller.ino
+++ servo_controller/servo_controller.ino
@@ -4,23 +4,64 @@
 
 #define LED_PIN 13
 
-Leg rightLeg(LEFT_FRONT_KNEE, LEFT_FRONT_HORI, LEFT_FRONT_VERT);
+Leg leg1(LEFT_FRONT_KNEE, LEFT_FRONT_HORI, LEFT_FRONT_VERT);
+Leg leg2(LEFT_MIDDLE_KNEE, LEFT_MIDDLE_HORI, LEFT_MIDDLE_VERT);
+Leg leg3(LEFT_REAR_KNEE, LEFT_REAR_HORI, LEFT_REAR_VERT);
+Leg leg4(RIGHT_FRONT_KNEE, RIGHT_FRONT_HORI, RIGHT_FRONT_VERT);
+Leg leg5(RIGHT_MIDDLE_KNEE, RIGHT_MIDDLE_HORI, RIGHT_MIDDLE_VERT);
+Leg leg6(RIGHT_REAR_KNEE, RIGHT_REAR_HORI, RIGHT_REAR_VERT);
 
 void setup(){
   pinMode(LED_PIN, OUTPUT);
-  rightLeg.initPosition(30,30,30);
+  leg1.initPosition(90,90,90);
+  leg2.initPosition(90,90,90);
+  leg3.initPosition(90,90,90);
+  leg4.initPosition(90,90,90);
+  leg5.initPosition(90,90,90);
+  leg6.initPosition(90,90,90);
 }
 
 int i= 0;
 void loop(){
-  if(i == 0) {
-    rightLeg.kneeMovement(150);
-    rightLeg.horizontalMovement(150);
-    rightLeg.verticalMovement(150);
+  if(i < 6) {
+    leg1.kneeMovement(150);
+    leg1.horizontalMovement(150);
+    leg1.verticalMovement(150);
+    leg2.kneeMovement(150);
+    leg2.horizontalMovement(150);
+    leg2.verticalMovement(150);
+    leg3.kneeMovement(150);
+    leg3.horizontalMovement(150);
+    leg3.verticalMovement(150);
+    leg4.kneeMovement(150);
+    leg4.horizontalMovement(150);
+    leg4.verticalMovement(150);
+    leg5.kneeMovement(150);
+    leg5.horizontalMovement(150);
+    leg5.verticalMovement(150);
+    leg6.kneeMovement(150);
+    leg6.horizontalMovement(150);
+    leg6.verticalMovement(150);
     delay(1000);
-    rightLeg.horizontalMovement(30);
-    rightLeg.kneeMovement(30); 
-    rightLeg.verticalMovement(30); 
+    leg1.horizontalMovement(30);
+    leg1.kneeMovement(30); 
+    leg1.verticalMovement(30);
+    leg2.horizontalMovement(30);
+    leg2.kneeMovement(30); 
+    leg2.verticalMovement(30); 
+    leg3.horizontalMovement(30);
+    leg3.kneeMovement(30); 
+    leg3.verticalMovement(30); 
+    leg4.horizontalMovement(30);
+    leg4.kneeMovement(30); 
+    leg4.verticalMovement(30); 
+    leg5.horizontalMovement(30);
+    leg5.kneeMovement(30); 
+    leg5.verticalMovement(30); 
+    leg6.horizontalMovement(30);
+    leg6.kneeMovement(30); 
+    leg6.verticalMovement(30);
+    delay(1000); 
     i++;
   }
   digitalWrite(LED_PIN, LOW);
@@ -30,5 +71,7 @@ void loop(){
 
 
 }
+
+
 
 
